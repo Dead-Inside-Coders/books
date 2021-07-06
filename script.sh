68e5dbcb5bcb0ae9b884b5148f135a0e63dc0005
egor-zavgorodnev <85026058+egor-zavgorodnev@users.noreply.github.com>
2021-07-06 13:38:30 +0300

    1

--- script.sh
+++ script.sh
@@ -1,18 +1,34 @@
-MAINBRANCH = "master"
+MAINBRANCH="main"
+EXTENTIONS=("_current" "_dev")
 
 is_merged_to_master() {
-    for branch in $(git for-each-ref --merged=origin/$MAINBRANCH --format="%(refname:short)" refs/heads/); do
+    for branch in $(git for-each-ref --merged=origin/${MAINBRANCH} --format="%(refname:short)" refs/heads/); do
 		if [ "$branch" == "$1" ]; then
-			return true; 
+			true
 		fi
 	done
-	return false;
+	false 
 }
 
 for branch in $(git show-ref --heads | sed 's/.*refs\/heads\///'); do
- if [ "$branch" != "$MAINBRANCH" ]; then
-    
+ echo "$branch"
+ if [ "$branch" != "${MAINBRANCH}" ]; then
+    if is_merged_to_master "$branch" ; 
+	then
+		echo "$branch: OK"
+	else 
+		echo "Error: $branch is not merged"
+		exit 1
+ 	fi
+	echo "All branches are merged to master!"
+	for extention in ${EXTENTIONS[@]}; do
+		if [ "$branch" == *"$extention"* ]; 
+		then
+			git branch -d $branch
+			echo "$branch successfully deleted!"
+		fi
+	done
  fi
 done
-
 read -p "Press Enter to continue ..." 
+
